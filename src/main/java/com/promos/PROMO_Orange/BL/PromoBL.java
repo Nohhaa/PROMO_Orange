2c2a51530cbaaf121b8a11cdec8325087c0a1f5c
Nohhaa <96923194+Nohhaa@users.noreply.github.com>
2023-07-24 10:47:00 +0300

    bl update

--- src/main/java/com/promos/PROMO_Orange/BL/PromoBL.java
+++ src/main/java/com/promos/PROMO_Orange/BL/PromoBL.java
@@ -1,14 +1,22 @@
 package com.promos.PROMO_Orange.BL;
 
-import com.promos.PROMO_Orange.Model.Requests;
+import com.promos.PROMO_Orange.Model.*;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PromoBL {
     private final Requests Req;
+    private final Customer Cus;
+    private final Fulfillment Ful;
+    private final Offer Off;
+    private final Segmentation Seg;
 
-    public PromoBL(Requests req) {
+    public PromoBL(Requests req, Customer cus, Fulfillment ful, Offer off, Segmentation seg) {
         Req = req;
+        Cus = cus;
+        Ful = ful;
+        Off = off;
+        Seg = seg;
     }
 
     public Requests getPromoRequest(int msisdn, int offerId) {
@@ -16,6 +24,17 @@ public class PromoBL {
         Req.setMsisdn(msisdn);
         System.out.println(Req.getMsisdn()+" "+Req.getofferId());
         return Req;
+
+    }
+    public void IsCustomer(int msisdn) {
+       // Cus.setId(Select id from Customer where Msisdn=msisdn);
+        Cus.setMsisdn(msisdn);
+        System.out.println(Req.getMsisdn()+" "+Req.getofferId());
+       // return;
+
+    }
+    public void IsSegmented(int msisdn) {
+
 
     }
 }
